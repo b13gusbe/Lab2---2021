a877c58e3f36e6461ac9100737df661f327e0efd
b13gusbe <gustav.bergstedt92@yahoo.se>
2021-09-09 00:53:19 +0200

    drawLogIn tillagd, inte klar.

--- Lab2 - Butik/Program.cs
+++ Lab2 - Butik/Program.cs
@@ -5,6 +5,9 @@ namespace Lab2___Butik
 {
     class Program
     {
+        static string p1 = "############################################################################\n";
+        static string p2 = "#                                                                          #\n";
+
         static void Main(string[] args) {
 
             Customer testCustomer = new Customer("Testson", "123");
@@ -17,23 +20,27 @@ namespace Lab2___Butik
             allProducts.Add(new Product("Banana", 20));
             allProducts.Add(new Product("Papaya", 25));
 
-
+            allCustomers.Add(testCustomer);
 
 
             //Console.WriteLine(allProducts[0].ToString());
-
+            /*
             int menuChoice = drawMenu();
 
+            if(menuChoice == 1) { drawLogIn(allCustomers); }
+            else if (menuChoice == 2) { }
+            */
 
+            drawLogIn(allCustomers);
         }
 
 
         private static int drawMenu()
         {
             ConsoleKeyInfo cki;
-            string p1 = "############################################################################\n";
-            string p2 = "#                                                                          #\n";
-
+            //string p1 = "############################################################################\n";
+            //string p2 = "#                                                                          #\n";
+            Console.Clear();
             Console.WriteLine(p1 + p2 + p2 + p2 + "#             Welcome to this \"online\" luxurious fruit store               #\n" + p2 + "#                                                                          #");
             Console.WriteLine("#             Please select one of the following alternetives:             #\n" + "#                                                                          #");
             Console.WriteLine("#             (1) Log in                                                   #");
@@ -49,6 +56,56 @@ namespace Lab2___Butik
                 } else if(cki.Key == ConsoleKey.D1){ return 1; }
                 else if(cki.Key == ConsoleKey.D2) { return 2; }
                 else if (cki.Key == ConsoleKey.D3) { Environment.Exit(0); }
+            } while (true);
+
+        }
+
+        private static void drawLogIn(List<Customer> allCustomers)
+        {
+            ConsoleKeyInfo cki;
+            Console.Clear();
+            Console.WriteLine(p1 + p2 + p2 + p2 + "#                       Please enter your username                         #\n" + p2 + p2 + p2 + p1);
+
+            do
+            {
+                string nameInput = Console.ReadLine();
+                Customer logedInCustomer = allCustomers.Find(x => x.username == nameInput);
+
+                if(logedInCustomer != null){
+
+                    Console.WriteLine("\nPlease enter password for {0}:", nameInput);
+                    string passwordInput = Console.ReadLine();
+
+
+                }
+                else if(nameInput != ""){
+                    Console.WriteLine("\nUnable to find {0}. Would you like to add this user? (Y/N)", nameInput);
+                    do
+                    {
+
+                        cki = Console.ReadKey();
+                        if(cki.Key == ConsoleKey.Y)
+                        {
+                            Console.WriteLine("\n\nChoose a password for new user {0}.", nameInput);
+                            string password = Console.ReadLine();
+                            Customer newCustomer = new Customer(nameInput, password);
+                            allCustomers.Add(newCustomer);
+                            Console.WriteLine("\nPassword has been set to: {0} \nYou are now able to log in with user: {1}\n\nPlease enter username: ", password, nameInput);
+                            break;
+                        }else if(cki.Key == ConsoleKey.N)
+                        {
+                            Console.WriteLine("\nEnter username: ");
+                            break;
+                        }else { Console.WriteLine("\nPlease enter Y or N"); }
+
+
+                    } while (true);
+
+                
+                }
+
+
+
             } while (true);
 
         }
